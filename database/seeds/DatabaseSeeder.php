4b68a35a45838df0eaaf69253644d8ea75f38cb1
vagrant <vagrant@oic-todo>
2018-07-02 04:43:33 +0000

    sort

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,12 +13,12 @@ class DatabaseSeeder extends Seeder
     {
         DB::table("tasks")->truncate();
         // $this->call(UsersTableSeeder::class);
-        DB::insert("insert into tasks (name,g) values ('愛すべきもの全てに高井田','200')");
+        DB::insert("insert into tasks (name,g) values ('愛すべきもの全てに高井田','250')");
         DB::insert("insert into tasks (name,g) values ('どかもりマッチョ','200')");
-        DB::insert("insert into tasks (name,g) values ('麺屋ガテン','200')");
-        DB::insert("insert into tasks (name,g) values ('歴史を刻め','200')");
+        DB::insert("insert into tasks (name,g) values ('麺屋ガテン','300')");
+        DB::insert("insert into tasks (name,g) values ('歴史を刻め','250')");
         DB::insert("insert into tasks (name,g) values ('男塾','200')");
         DB::insert("insert into tasks (name,g) values ('好きだから','200')");
-        DB::insert("insert into tasks (name,g) values ('のスタ','200')");
+        DB::insert("insert into tasks (name,g) values ('のスタ','300')");
     }
 }
